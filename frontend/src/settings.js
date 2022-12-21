0cfa5eafab639009a78cbc7fd8abf8d0c18f0f1c
ddout <ddout@qq.com>
2022-12-21 16:05:00 +0800

    修改小版本号1.17.0.001

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   TokenKey: 'Authorization',
   RefreshTokenKey: 'refreshauthorization',
   LinkTokenKey: 'LINK-PWD-TOKEN',
-  title: 'DataEase-1.16.0.001',
+  title: 'DataEase-1.17.0.001',
   /* for sso */
   IdTokenKey: 'IdToken',
   AccessTokenKey: 'AccessToken',
